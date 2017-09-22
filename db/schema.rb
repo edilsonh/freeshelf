14348eaa2d281b4de24a00d2a27753b3ffe6e23b
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-22 15:49:26 -0400

    added association between books and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830174551) do
+ActiveRecord::Schema.define(version: 20170922185117) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170830174551) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
